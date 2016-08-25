56ee43a3a41e03e146521bb53cf2874bba125116
LuukVermunt <luukvermunt@hotmail.com>
2016-08-25 10:58:10 +0200

    Update anaMuJetsSkimTree.h

--- anaMuJetsSkimTree.h
+++ anaMuJetsSkimTree.h
@@ -16,57 +16,51 @@ class anaMuJetsSkimTree{
 
 public:
 
+  TTree* tr_[5];     //Change, or think of something, when more files are included
+  TRandom3* rand = new TRandom3();
+  bool JetSmearing = true;
+
   anaMuJetsSkimTree();
-  ~anaMuJetsSkimTree();
+  ~anaMuJetsSkimTree(); 
 
   //Interface functions
+  void SetCSVCut(double CSVcut) {/*cout << "Changing CSVCut from " << CSVCut_ << " to " << CSVcut << endl*/CSVCut_ = CSVcut; };
+  void SetnumbB(double numbB) {cout << "Changing number b-jets Cut from " << numbB_ << " to " << numbB << endl; numbB_ = numbB; };
+  void SetnumbJ(double numbJets) {cout << "Changing number jets Cut from " << numbJets_ << " to " << numbJets << endl; numbJets_ = numbJets; };
+  void SetjetEtaCut(double jtEtaCut) {cout << "Changing jtEta Cut from " << jtEtaCut_ << " to " << jtEtaCut << endl; jtEtaCut_ = jtEtaCut; }
+  
+  double GetCSVCut() {return CSVCut_; };
+  int GethiBin() {return hiBin; };
+  double GetjetPt(int index) {return jetPt[index]; };
+  double GetlepPt(int index) {return lepPt[index]; };
+  double GetlepIso(int index) {return lepIso[index]; };
+  double GetjetCSVv1(int index) {return discr_csvV1[index]; };
+  double GetjetCSVv2(int index) {return discr_csvV2[index]; };
+  int GetnLep() {return nLep; };
+
   void OpenFiles(TString FileName, int i);
   void BuildHistograms();
-  void CalculateNormalizationHistograms(TH1F* h[4], int option);
-  void NormalizeHistograms(int option, int Drawoption);
+  int FindLeadingMuonAfterCuts(int option);
+  std::vector<int> FindJetsAfterCuts(int indexMuon);
+  std::vector<int> FindbJets(std::vector<int> indexJets);
+  std::vector<int> FindbJetsCSVv2(std::vector<int> indexJets);
+  void CalculateNormalizationHistograms(TH1F* h[4], int option, int nBjets);
+  void NormalizeHistograms(int option, int nBjets);
+  void NormalizeHistogramsToOne(int i, int nBjets);
   double CalculateDeltaPhi(int ilep, int index_b);
   void SetAddressBranches(int i);
   std::vector<int> BuildCSVVectorLeadingbJets(std::vector<int> indexJets);
-  void FillHistogramsBeforeCuts(int option, int i);
-  void FillHistogramsAfterCuts(int option, int i);
-  void LayoutHistograms(int option);
-  TCanvas* PlotHistograms(int option2);
+  std::vector<int> BuildCSVVectorLeadingbJetsCSVv2(std::vector<int> indexJets);
+  std::vector<int> BuildpTVectorLeadingbJets(std::vector<int> indexbJets);
+  void FillHistogramsBeforeCuts(int option);
+  void FillHistogramsAfterCuts(int option);
 
 private:
   //Constant variables
   const Float_t muM = .1056583715;
 
   //Objects
-  TFile* f_[21];      //Change, or think of something, when more files are included
-  TTree* tr_[21];     //Change, or think of something, when more files are included
-
-  //4 types of files: Data, MC tt, MC W, MC DY
-  TH1F* h_events_[4];
-  TH1F* h_totevents[4];
-
-  TH1F* h_lepPt_[4];
-  TH1F* h_lepPseu_[4];
-  TH1F* h_jetHt_[4];
-  TH1F* h_jetCSV_[4];
-  //TH1F* h_lepbjetMinv_[4];
-  TH1F* h_lepbjetMinv_min_[4];
-  //TH1F* h_ttMinv_[4];
-  TH1F* h_ttMinv_min_[4];
-  TH1F* h_Phi_1stb_[4];
-  TH1F* h_Phi_allb_[4];
-  TH1F* h_Phi_b_minpi_[4];
-  TH2F* h_2dCSV_[4];
-  TH2F* h_2dCSV2_[4];
-  TGraph* CSV_effcut;
-  TGraph* CSV_effcut2;
-
-  THStack* lepPt_Stack;
-  THStack* lepPseu_Stack;
-  THStack* jetHt_Stack;
-  THStack* jetCSV_Stack;
-  THStack* lepbjetMinv_Stack;
-  THStack* ttMinv_Stack;
-
+  TFile* f_[5];      //Change, or think of something, when more files are included
 
   //Global variables
   unsigned int run_, lumi_;
@@ -92,11 +86,17 @@ private:
   float jtEta[500];
   float jtM[500];
   float discr_csvV1[500];
-
+  float discr_csvV2[500];
 
   //Cut values
-  double CSVCut1_;
-  double CSVCut2_;
+  int numbB_ = 1;
+  int numbJets_ = 4;
+  double CSVCut_ = 0.75;
+  double mupTCut_ = 18;
+  double jtpTCut_ = 30.;
+  double jtEtaCut_ = 2.;
+  double drJetToMuonCut_ = 0.3;
+  double WMassHighCut_ = 170.;
 };
 
 #endif
